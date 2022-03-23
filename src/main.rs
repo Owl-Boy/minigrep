5b8b6c989eaea79d17cdc9754e68358f5fcea3b9
Owl-Boy <sharmashubh223@gmail.com>
2022-03-23 08:32:35 +0530

    refactored the code

--- src/main.rs
+++ src/main.rs
@@ -4,14 +4,26 @@ use std::fs;
 fn main() {
     let args: Vec<String> = env::args().collect();
 
-    let query = &args[1];
-    let filename = &args[2];
+    let config = parse_config(&args);
 
-    println!("Searching for {}", query);
-    println!("In file {}", filename);
+    println!("Searching for {}", config.query);
+    println!("In file {}", config.filename);
 
-    let contents = fs::read_to_string(filename)
+    let contents = fs::read_to_string(config.filename)
         .expect("something went wrong reading the file");
 
     println!("With text:\n{}", contents);
+}
+
+#[derive(Debug)]
+struct Config {
+    query: String,
+    filename: String,
+}
+
+fn parse_config(args: &[String]) -> Config {
+    let query = args[1].clone();
+    let filename = args[2].clone();
+
+    Config { query, filename }
 }
